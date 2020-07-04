d243a6e152cf72c0c69b5f1fe4f4031b946d3651
Hanson Sin <46597093+HansonSin@users.noreply.github.com>
2020-07-04 10:06:53 +0800

    Add files via upload

---
+++ pirate.py
@@ -0,0 +1,15 @@
+fin = open("piratein.txt", "r")
+
+lis = []
+
+for i in range(3):
+
+    lis.append(int(fin.readline()))
+
+fin.close()
+
+fout = open("pirateout.txt", "w")
+
+fout.write(str(min(lis[1] + lis[2], lis[0] - lis[1] + lis[0] - lis[2])))
+
+fout.close()
